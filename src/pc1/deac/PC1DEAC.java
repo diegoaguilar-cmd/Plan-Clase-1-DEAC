d68feb206b0eb71694d7e9fa956cd942b6f5ad17
Diego <Diegoenriqueaguilar@emilianisomascos.edu.gt>
2026-01-20 11:00:44 -0600

    Holas Mundos

---
+++ src/pc1/deac/PC1DEAC.java
@@ -0,0 +1,17 @@
+package pc1.deac;
+
+/**
+ *
+ * @author HP
+ */
+public class PC1DEAC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo Java");
+        
+    }
+    
+}
