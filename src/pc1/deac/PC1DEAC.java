7a5bb49b44bffd5be85cc998f2b22c38760de0d1
Diego <Diegoenriqueaguilar@emilianisomascos.edu.gt>
2026-01-20 11:07:56 -0600

    Iniciales

--- src/pc1/deac/PC1DEAC.java
+++ src/pc1/deac/PC1DEAC.java
@@ -10,7 +10,7 @@ public class PC1DEAC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo Java");
+        System.out.println("Hola mundo Java DEAC");
         
     }
     
