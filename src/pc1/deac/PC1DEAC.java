8dbd0737803131704971629a14b82b4d31611898
Diego <Diegoenriqueaguilar@emilianisomascos.edu.gt>
2026-01-20 11:03:43 -0600

    Computos

--- src/pc1/deac/PC1DEAC.java
+++ src/pc1/deac/PC1DEAC.java
@@ -12,6 +12,8 @@ public class PC1DEAC {
     public static void main(String[] args) {
         System.out.println("Hola mundo Java");
         System.out.println("Quinto");
+        System.out.println("Computacion");
+        
         
     }
     
